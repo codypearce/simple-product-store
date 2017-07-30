47fe79d74bfad5ed028503da7f0a340495383642
codypearce <codyapearce@gmail.com>
2017-07-29 23:20:34 -0600

    set up admin route and index

--- api/routes.js
+++ api/routes.js
@@ -2,6 +2,9 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('index');
     });
+	app.get('/admin', function(req, res) {
+    	res.render('admin/index');
+    });
     app.get('*', function(req, res) {
         res.render('error');
     });
