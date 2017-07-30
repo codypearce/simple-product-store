cf1869e9b13720a038afd5c71f654454d0b5638d
codypearce <codyapearce@gmail.com>
2017-07-29 23:24:39 -0600

    make admin layout, add route

--- api/routes.js
+++ api/routes.js
@@ -2,9 +2,16 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('index');
     });
+
+
 	app.get('/admin', function(req, res) {
     	res.render('admin/index');
     });
+    app.get('/admin/product/add', function(req, res) {
+    	res.render('admin/add');
+    });
+
+
     app.get('*', function(req, res) {
         res.render('error');
     });
