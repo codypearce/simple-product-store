7f447f1dae2e32c8d2a73e27e2138d61f4f6558b
codypearce <codyapearce@gmail.com>
2017-07-30 19:15:52 -0600

    create product post route

--- api/routes.js
+++ api/routes.js
@@ -1,3 +1,5 @@
+var Product = require('./models/product');
+
 module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('index');
@@ -10,6 +12,22 @@ module.exports = function(app) {
     app.get('/admin/product/add', function(req, res) {
     	res.render('admin/add');
     });
+    app.post('/products', function(req, res) {
+        console.log(req.body)
+        Product.create({
+            title: req.body.title,
+            slug: req.body.slug,
+            price: req.body.price,
+            link: req.body.link,
+            image: req.body.image,
+            description: req.body.description 
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+            res.render('index')
+        });
+
+    })
 
 
     app.get('*', function(req, res) {
