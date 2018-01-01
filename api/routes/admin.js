db2e57027a90f8ea6ead0bd897aef42a9d7d102b
codypearce <outis161@gmail.com>
2017-12-31 21:11:42 -0500

    img file name is set, filesize is limited to 2mb, check to make sure file is image, link product to image

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,19 +1,35 @@
 var express = require('express'),
     router = express.Router(),
     Product = require('../models/product')
-
+var path = require('path');
 var multer  = require('multer');
 var dest = 'client/productImages/';
 var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, dest);
-    },
+    destination: dest,
     filename: function (req, file, cb) {
-        cb(null, file.originalname);
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 })
-var upload = multer({ storage: storage })
+var upload = multer({
+    storage: storage,
+    limits: {fileSize: 2000000},
+    fileFilter: function(req, file, cb) {
+        checkFileType(file, cb)
+    }
+}).single('imageUpload');
+function checkFileType(file, cb) {
+    // Allowed Ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
 
+    const mimetype = filetypes.test(file.mimetype);
+
+    if(mimetype && extname) {
+        return cb(null, true);
+    } else {
+        return cb('Error: Images only')
+    }
+}
 router.get('/', function(req, res) {
   Product.find(function(err, products) {
       limitText(products, 100)
@@ -24,22 +40,33 @@ router.get('/', function(req, res) {
 });
 
 
-router.post('/products', upload.single('imageUpload'), function (req, res) {
-    Product.create({
-        title: req.body.title,
-        slug: req.body.slug,
-        price: req.body.price,
-        link: req.body.link,
-        imgLink: req.body.imgLink,
-        description: req.body.description
-    }, function(err, product) {
+router.post('/products', function (req, res) {
+    let imgName;
+    upload(req, res, (err) => {
+        if(err) {
+            return console.log(err)
 
-        if (err) res.send(err);
+        } else {
 
-        Product.find(function(err, products) {
-            res.render('admin/index', { products: products })
-        })
-    });
+            imgName = req.file.filename;
+            Product.create({
+                title: req.body.title,
+                slug: req.body.slug,
+                price: req.body.price,
+                link: req.body.link,
+                imgLink: imgName,
+                description: req.body.description
+            }, function(err, product) {
+
+                if (err) res.send(err);
+
+                Product.find(function(err, products) {
+                    res.render('admin/index', { products: products })
+                })
+            });
+        }
+    })
+
 })
 
 router.get('/product/add', function(req, res) {
@@ -54,7 +81,7 @@ router.get('/product/edit/:productID', (req,res) => {
   })
 });
 
-router.post('/product/edit/:productID', upload.single('imageUpload'), function(req, res) {
+router.post('/product/edit/:productID', function(req, res) {
     Product.update({_id: req.params.productID}, {
         title: req.body.title,
         slug: req.body.slug,
