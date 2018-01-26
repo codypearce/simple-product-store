290cced5fcee6331521e44df78350ca03771f1d4
codypearce <outis161@gmail.com>
2018-01-25 21:21:05 -0700

    add required in product schema, add trim validator

--- api/models/product.js
+++ api/models/product.js
@@ -2,12 +2,11 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var productSchema = new Schema({
-    title: String,
-    description: String,
-    slug: {type: String, unique: true, required: true},
-    externalLink: String,
-    price: Number,
-    link: String
+    title: {type: String, required: true, trim: true},
+    description: {type: String, required: true},
+    slug: {type: String, unique: true, required: true, trim: true},
+    externalLink: {type: String, trim: true},
+    price: {type: Number, required: true}
 }, {
     timestamps: true
 })
