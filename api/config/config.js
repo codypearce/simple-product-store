089d44157f4eac14babfad720c03d13686bbdf40
codypearce <outis161@gmail.com>
2018-01-29 20:59:48 -0700

    Add config, set up test database, fix globals

---
+++ api/config/config.js
@@ -0,0 +1,8 @@
+let config = {}
+
+config.mongoURI = {
+    dev: 'mongodb://localhost/simple-product-store',
+    test: 'mongodb://localhost/simple-product-store-test'
+}
+
+module.exports = config
--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -1,6 +1,13 @@
-var mongoose = require('mongoose')
-
+let mongoose = require('mongoose')
+const config = require('../config/config')
 // Set up Database
-mongoose.connection.openUri('mongodb://localhost/simple-product-store')
+
+let mongoURI
+if (process.env.NODE_ENV === 'dev') {
+    mongoURI = config.mongoURI.dev
+} else {
+    mongoURI = config.mongoURI.test
+}
+mongoose.connection.openUri(mongoURI)
 
 module.exports = {mongoose}
