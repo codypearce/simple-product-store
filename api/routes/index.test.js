59dcce432b436992ef7cdf9761505360343db7a1
codypearce <outis161@gmail.com>
2018-01-30 21:11:15 -0700

    Add index route test

---
+++ api/routes/index.test.js
@@ -0,0 +1,20 @@
+const chai = require('chai')
+const expect = chai.expect
+const chaiHttp = require('chai-http')
+
+chai.use(chaiHttp)
+
+const { app } = require('../../app')
+
+describe('Index', () => {
+    it('should return with homepage', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                if (err) throw err
+                expect(res).to.have.status(200)
+                expect(res.body).to.not.equal(null)
+                done()
+            })
+    })
+})
