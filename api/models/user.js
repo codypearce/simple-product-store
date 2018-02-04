f7b4e22ee7f3dcfaca331f31423809c8dad86bd9
codypearce <outis161@gmail.com>
2018-02-03 20:52:15 -0700

    set up user model, add validtor library for email

---
+++ api/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const validator = require('validator')
+
+var userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minLength: 4
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema)
