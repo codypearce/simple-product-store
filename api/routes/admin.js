144dbcd566bd9396d99750c18534035cc9b14bc3
codypearce <outis161@gmail.com>
2018-02-25 20:30:29 -0700

    move uploadfiles to own file

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,38 +1,7 @@
-const express = require('express')
 const Product = require('../models/product')
 const User = require('../models/user')
-const path = require('path')
+const {upload} = require('./uploadFiles')
 const {isLoggedIn, limitText} = require('./utils')
-
-var multer = require('multer')
-var dest = 'client/productImages/'
-var storage = multer.diskStorage({
-    destination: dest,
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-    }
-})
-var upload = multer({
-    storage: storage,
-    limits: {fileSize: 2000000},
-    fileFilter: function (req, file, cb) {
-        checkFileType(file, cb)
-    }
-}).single('imageUpload')
-
-function checkFileType (file, cb) {
-    // Allowed Ext
-    const filetypes = /jpeg|jpg|png|gif/
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
-
-    const mimetype = filetypes.test(file.mimetype)
-
-    if (mimetype && extname) {
-        return cb(null, true)
-    } else {
-        return cb('Error: Images only')
-    }
-}
 
 module.exports = function (app, passport) {
     app.get('/admin', isLoggedIn, function (req, res) {
