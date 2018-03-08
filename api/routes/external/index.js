3ac0c587064db30f408ba7f2b5b01781717672b9
codypearce <outis161@gmail.com>
2018-03-07 20:03:11 -0800

    pagination on index page with products

--- api/routes/external/index.js
+++ api/routes/external/index.js
@@ -2,13 +2,22 @@ const Product = require('../../models/product')
 
 module.exports = function (app, passport) {
     app.get('/', function (req, res) {
+        let perPage = 6
+        let page = Math.max(0, req.param('page'))
         Product.find({})
             .sort({'createdAt': -1})
-            .limit(20)
+            .skip(perPage * (page - 1))
+            .limit(perPage)
             .exec(function (err, products) {
                 if (err) console.log(err)
-
-                res.render('index', { products })
+                Product.count().exec(function (err, count) {
+                    if (err) console.log(err)
+                    res.render('index', {
+                        products,
+                        page: page,
+                        pages: count / perPage
+                    })
+                })
             })
     })
 
