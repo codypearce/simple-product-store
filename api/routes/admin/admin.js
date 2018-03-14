b4a0cbea3e28e23d78e2cf1d8301aa587d5443e7
codypearce <outis161@gmail.com>
2018-03-13 23:28:22 -0700

    Set up paginiation on admin

--- api/routes/admin/admin.js
+++ api/routes/admin/admin.js
@@ -1,20 +1,35 @@
 const Product = require('../../models/product')
-
+const Setting = require('../../models/setting')
 const {isLoggedIn, limitText} = require('../utils')
 
 module.exports = function (app, passport) {
     app.get('/admin', isLoggedIn, function (req, res) {
-        Product.find(function (err, products) {
-            if (err) res.send(err)
-
-            limitText(products, 100)
-
-            let sorted = products.sort(function (a, b) {
-                return new Date(b.createdAt) - new Date(a.createdAt)
+        Setting.find({}, function (err, settings) {
+            if (err) console.log(err)
+            let perPage
+            settings.forEach(item => {
+                if (item.humanName === 'Products Per Page') {
+                    perPage = item.value
+                }
             })
-            res.render('admin/index', {
-                products: sorted
-            })
+
+            let page = req.param('page') || 1
+            Product.find({})
+                .sort({'createdAt': -1})
+                .skip(perPage * (page - 1))
+                .limit(perPage)
+                .exec(function (err, products) {
+                    if (err) console.log(err)
+                    limitText(products, 100)
+                    Product.count().exec(function (err, count) {
+                        if (err) console.log(err)
+                        res.render('admin/index', {
+                            products,
+                            page: page,
+                            pages: Math.ceil(count / perPage)
+                        })
+                    })
+                })
         })
     })
 }
