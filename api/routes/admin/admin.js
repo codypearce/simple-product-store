692c15bcd1de371d95a6ce09dbc157581f47148a
codypearce <outis161@gmail.com>
2018-03-15 22:50:20 -0700

    implement simple search with regex on admin

--- api/routes/admin/admin.js
+++ api/routes/admin/admin.js
@@ -4,33 +4,64 @@ const {isLoggedIn, limitText} = require('../utils')
 
 module.exports = function (app, passport) {
     app.get('/admin', isLoggedIn, function (req, res) {
-        Setting.find({}, function (err, settings) {
-            if (err) console.log(err)
-            let perPage
-            settings.forEach(item => {
-                if (item.humanName === 'Products Per Page') {
-                    perPage = item.value
-                }
-            })
+        if (req.query.search) {
+            Setting.find({}, function (err, settings) {
+                if (err) console.log(err)
+                let perPage
+                settings.forEach(item => {
+                    if (item.humanName === 'Products Per Page') {
+                        perPage = item.value
+                    }
+                })
 
-            let page = req.param('page') || 1
-            Product.find({})
-                .sort({'createdAt': -1})
-                .skip(perPage * (page - 1))
-                .limit(perPage)
-                .exec(function (err, products) {
-                    if (err) console.log(err)
-                    limitText(products, 100)
-                    Product.count().exec(function (err, count) {
+                let page = req.param('page') || 1
+                Product.find({ 'title': { $regex: req.query.search, $options: 'i' }})
+                    .sort({'createdAt': -1})
+                    .skip(perPage * (page - 1))
+                    .limit(perPage)
+                    .exec(function (err, products) {
                         if (err) console.log(err)
-                        res.render('admin/index', {
-                            products,
-                            page: page,
-                            pages: Math.ceil(count / perPage),
-                            count
+                        limitText(products, 100)
+                        Product.count().exec(function (err, count) {
+                            if (err) console.log(err)
+                            res.render('admin/index', {
+                                products,
+                                page: page,
+                                pages: Math.ceil(count / perPage),
+                                count
+                            })
                         })
                     })
+            })
+        } else {
+            Setting.find({}, function (err, settings) {
+                if (err) console.log(err)
+                let perPage
+                settings.forEach(item => {
+                    if (item.humanName === 'Products Per Page') {
+                        perPage = item.value
+                    }
                 })
-        })
+
+                let page = req.param('page') || 1
+                Product.find({})
+                    .sort({'createdAt': -1})
+                    .skip(perPage * (page - 1))
+                    .limit(perPage)
+                    .exec(function (err, products) {
+                        if (err) console.log(err)
+                        limitText(products, 100)
+                        Product.count().exec(function (err, count) {
+                            if (err) console.log(err)
+                            res.render('admin/index', {
+                                products,
+                                page: page,
+                                pages: Math.ceil(count / perPage),
+                                count
+                            })
+                        })
+                    })
+            })
+        }
     })
 }
